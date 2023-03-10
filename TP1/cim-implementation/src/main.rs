23314c05d25dcabf8baa8f8e3d2e36106cbbd823
Alan Sartorio <alan42ga@hotmail.com>
2023-03-10 12:11:40 -0300

    feat: implement input parser

--- TP1/cim-implementation/src/main.rs
+++ TP1/cim-implementation/src/main.rs
@@ -1,21 +1,22 @@
-use cgmath::Vector2;
+use cgmath::{vec2, Vector2};
+use chumsky::{prelude::*, text::newline};
 use std::{
     collections::HashMap,
-    error::Error,
     fmt::{Display, Write},
     io::{stdin, Read},
     iter,
-    str::FromStr,
 };
 
 type ID = usize;
 
+#[derive(Debug)]
 struct Particle {
     id: ID,
     position: Vector2<f64>,
     radius: f64,
 }
 
+#[derive(Debug)]
 struct ParticlesData {
     n: usize,
     l: f64,
@@ -24,12 +25,49 @@ struct ParticlesData {
     particles: Vec<Particle>,
 }
 
-impl FromStr for ParticlesData {
-    type Err = Box<dyn Error>;
+fn parser<'a>() -> impl Parser<'a, &'a str, ParticlesData, extra::Err<Rich<'a, char>>> {
+    let digits = text::digits(10);
+    let unsigned = digits.map_slice(|s: &str| s.parse::<usize>().unwrap());
 
-    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-        todo!("Implement parser");
-    }
+    let num = just('-')
+        .or_not()
+        .then(text::int(10))
+        .then(just('.').then(digits).or_not())
+        .map_slice(|s: &str| s.parse().unwrap());
+
+    let particle_data = unsigned
+        .then_ignore(just(' '))
+        .then(num.separated_by_exactly::<_, _, 3>(just(' ')))
+        .map(|(id, [x, y, r])| Particle {
+            id,
+            position: vec2(x, y),
+            radius: r,
+        });
+
+    let particles = particle_data
+        .separated_by(newline())
+        .at_least(1)
+        .allow_trailing()
+        .collect();
+
+    unsigned
+        .then_ignore(newline())
+        .then(num)
+        .then_ignore(newline())
+        .then(unsigned)
+        .then_ignore(newline())
+        .then(num)
+        .map(|(((n, l), m), r_c)| (n, l, m, r_c))
+        .then_ignore(newline())
+        .then(particles)
+        .map(|((n, l, m, r_c), particles)| ParticlesData {
+            n,
+            l,
+            m,
+            r_c,
+            particles,
+        })
+        .then_ignore(end())
 }
 
 struct NeighborMap {
@@ -61,7 +99,12 @@ impl ParticlesData {
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
-    let input: ParticlesData = input.parse().expect("Error parsing input data.");
+    let input: ParticlesData = parser()
+        .parse(&input)
+        .into_result()
+        .expect("Error parsing input data.");
+
+    dbg!(&input);
 
     let output = input.generate_neighbor_map();
 
