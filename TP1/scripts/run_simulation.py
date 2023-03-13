716e90b0e3512a6f24c568854ec888a7167f7365
Lucas Vittor <lvvittor@gmail.com>
2023-03-12 21:04:23 -0300

    chore: rename file

---
+++ TP1/scripts/run_simulation.py
@@ -0,0 +1,56 @@
+from generate import generate
+from typing import Optional
+import subprocess
+
+def write_file(n: int, l: float, m: int, rc: float, seed: Optional[int] = None) -> None:
+    file_string = generate(n, l, m, rc, seed)
+    with open("/tmp/temp.txt", "w") as f:
+        f.write(file_string)
+
+
+# Decorator for running the function run_cim multiple times and get the average
+def run_multiple_times(times: int):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            total = 0
+            for _ in range(times):
+                total += func(*args, **kwargs)
+            average_time = total / times
+            return average_time
+        return wrapper
+    return decorator
+    
+
+@run_multiple_times(times=10)
+def run_cim(input_data: str, cyclic: bool = False, brute_force: bool = False):
+    args = ""
+
+    if cyclic:
+        args += "--cyclic "
+    if brute_force:
+        args += "--brute-force "
+    
+    process = subprocess.Popen(["make", "-s", "-C", "cim-implementation",
+                                "run-impl", f"INPUT_FILE_PATH=/dev/stdin", f"ARGS={args}"], stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    _, stderr = process.communicate(input=input_data.encode())
+    return float(stderr)
+
+
+def q2():
+    L = 20
+    Rc = 1
+    r = .25
+
+    for N in [100, 200, 400, 800, 1600]:
+        for M in [1, 2, 4, 8, 16]:
+            input_data = generate(N, L, M, Rc, r_min=r, r_max=r, seed=0)
+            print("=====================================")
+            print(f"N={N}, M={M}")
+            print(f"cyclic={True}, brute_force={True}, average_time={run_cim(input_data, cyclic=True, brute_force=True)}")
+            print(f"cyclic={True}, brute_force={False}, average_time={run_cim(input_data, cyclic=True, brute_force=False)}")
+            print(f"cyclic={False}, brute_force={True}, average_time={run_cim(input_data, cyclic=False, brute_force=True)}")
+            print(f"cyclic={False}, brute_force={False}, average_time={run_cim(input_data, cyclic=False, brute_force=False)}")
+            print("=====================================")
+
+if __name__ == "__main__":
+    q2()
