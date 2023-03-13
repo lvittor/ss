b46e10cf99434729947b9bb59ab852918f0f4e98
Lucas Vittor <lvvittor@gmail.com>
2023-03-13 01:08:06 -0300

    feat: add question 3

--- TP1/scripts/run_simulation.py
+++ TP1/scripts/run_simulation.py
@@ -1,3 +1,4 @@
+from math import pi
 from generate import generate
 from typing import Optional
 import subprocess
@@ -52,5 +53,32 @@ def q2():
             print(f"cyclic={False}, brute_force={False}, average_time={run_cim(input_data, cyclic=False, brute_force=False)}")
             print("=====================================")
 
+
+def q3():
+    L = 20
+    Rc = 1
+    r = .25
+
+    for N in [100, 200, 400, 800, 1600]:
+        avg_times_1 = []
+        avg_times_2 = []
+        print("=====================================")
+        for M in range(1, L // Rc):
+            input_data = generate(N, L, M, Rc, r_min=r, r_max=r, seed=0)
+            avg_1 = run_cim(input_data, cyclic=True, brute_force=False)
+            avg_2 = run_cim(input_data, cyclic=False, brute_force=False)
+            avg_times_1.append([M, avg_1])
+            avg_times_2.append([M, avg_2])
+            print(f"N={N}, M={M}")
+            print(f"cyclic={True}, brute_force={False}, average_time={avg_1}")
+            print(f"cyclic={False}, brute_force={False}, average_time={avg_2}")
+        print("-------------------------------------")
+        print(f"Optimal M for cyclic={True}, brute_force={False}: (M, time)={min(avg_times_1, key=lambda x: x[1])}")
+        print(f"Optimal M for cyclic={False}, brute_force={False}: (M, time)={min(avg_times_2, key=lambda x: x[1])}")
+        print("=====================================")
+
+            
+
 if __name__ == "__main__":
     q2()
+    # q3()
