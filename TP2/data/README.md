48229381f779181b239e0395c9d9e5c7e440bca6
Alan Sartorio <alan42ga@hotmail.com>
2023-03-17 12:17:43 -0300

    chore: define tp2 input format

---
+++ TP2/data/README.md
@@ -0,0 +1,16 @@
+## Input and Output data
+
+Input data contains the following format:
+
+```
+RNG_seed | any
+N
+L
+Rc
+Noise
+id1 x1 y1 vx1 vy1
+id2 x2 y2 vx2 vy2
+id3 x3 y3 vx3 vy3
+...
+idN xN yN vxN vyN
+```
---
+++ TP2/scripts/generate.py
@@ -0,0 +1,21 @@
+import random
+from typing import Optional
+import math
+
+
+def generate(n: int, l: float, rc: float, noise: float, speed: float, runtime_seed: Optional[int] = None, seed: Optional[int] = None) -> str:
+    if seed is not None:
+        random.seed(seed)
+    noise_seed = 'any' if runtime_seed is None else str(runtime_seed)
+    out = '\n'.join(map(str, [noise_seed, n, l, rc, noise])) + '\n'
+
+    for i in range(n):
+        angle = random.uniform(-math.pi, math.pi)
+        out += ' '.join(map(str, [i, random.uniform(0, l),
+                        random.uniform(0, l), math.cos(angle) * speed, math.sin(angle) * speed])) + '\n'
+
+    return out
+
+
+if __name__ == "__main__":
+    print(generate(300, 7.0, 1.0, 2.0, .03))
