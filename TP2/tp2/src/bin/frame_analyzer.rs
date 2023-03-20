c242026ea4d6087803af1bf094b4fa665cfc91e6
Alan Sartorio <alan42ga@hotmail.com>
2023-03-20 10:56:13 -0300

    feat: add analyzer to calculate va for each frame

---
+++ TP2/tp2/src/bin/frame_analyzer.rs
@@ -0,0 +1,53 @@
+use std::{
+    fs::{read_to_string, File},
+    io::{BufRead, BufReader, Write},
+    path::PathBuf,
+};
+
+use chumsky::Parser;
+use clap::Parser as _parser;
+use nalgebra::Vector2;
+use tp2::parser::{input_parser, output_parser};
+
+#[derive(clap::Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    input: PathBuf,
+
+    #[arg(short, long)]
+    output: PathBuf,
+
+    #[arg(short, long)]
+    analysis: PathBuf,
+
+    #[arg(long)]
+    capture_directory: Option<PathBuf>,
+}
+
+fn main() {
+    let args = Args::parse();
+    let input = read_to_string(args.input).unwrap();
+    let output_file = File::open(args.output).unwrap();
+    let system_info = input_parser()
+        .parse(&input)
+        .into_result()
+        .expect("Error parsing input data.");
+
+    let mut analysis_file = File::create(args.analysis).unwrap();
+
+    for frame in output_parser(
+        system_info.particles.len(),
+        BufReader::new(output_file).lines(),
+    ) {
+        let va = frame
+            .particles
+            .iter()
+            .map(|p| p.velocity_direction)
+            .sum::<Vector2<f64>>()
+            .magnitude()
+            / frame.particles.len() as f64;
+
+        analysis_file.write_fmt(format_args!("{va}\n")).unwrap();
+    }
+}
