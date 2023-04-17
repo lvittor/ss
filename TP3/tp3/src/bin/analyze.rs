9d0ddc72c20291b7896522fb7ac42d83b42678d4
Alan Sartorio <alan42ga@hotmail.com>
2023-04-17 16:49:01 -0300

    feat(tp3): add analyzer for processing output data

---
+++ TP3/tp3/src/bin/analyze.rs
@@ -0,0 +1,50 @@
+use std::{
+    fs::{read_to_string, File},
+    io::{BufRead, BufReader, Write},
+    path::PathBuf,
+};
+
+use chumsky::Parser;
+use clap::Parser as _parser;
+use tp3::{
+    parser::{input_parser, output_parser},
+    particle::Frame,
+};
+
+#[derive(clap::Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    input: PathBuf,
+
+    #[arg(short, long)]
+    output: PathBuf,
+
+    #[arg(short, long)]
+    analysis: PathBuf,
+}
+
+fn main() {
+    let args = Args::parse();
+    let input = read_to_string(args.input).unwrap();
+    let output_file = File::open(args.output).unwrap();
+    let system_info = input_parser()
+        .parse(&input)
+        .into_result()
+        .expect("Error parsing input data.");
+
+    let mut analysis_file = File::create(args.analysis).unwrap();
+
+    for frame in output_parser(BufReader::new(output_file).lines()) {
+        let Frame { time, balls } = frame;
+        let energy: f64 = balls
+            .iter()
+            .map(|p| 0.5 * system_info.ball_mass * p.velocity.magnitude().powi(2))
+            .sum();
+        let ball_count = balls.len();
+
+        analysis_file
+            .write_fmt(format_args!("{time},{ball_count},{energy}\n"))
+            .unwrap();
+    }
+}
