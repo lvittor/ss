9e080ab1826470deb456270cc3d2e4546fa1ba7c
Alan Sartorio <alan42ga@hotmail.com>
2023-04-17 18:45:48 -0300

    feat(tp3): implement run_simulation

--- TP3/scripts/run_simulation.py
+++ TP3/scripts/run_simulation.py
@@ -1,10 +1,12 @@
 from io import BytesIO, StringIO
 from math import pi
+from multiprocessing.pool import ThreadPool
 from generate import generate
 from typing import Optional, TextIO
 import subprocess
 import pandas as pd
 import numpy as np
+from multiprocessing import Pool
 
 # Decorator for running the function run_cim multiple times and get the average
 
@@ -13,16 +15,16 @@ def run_multiple_times(times: int):
     def decorator(func):
         def wrapper(input_generator):
             df = pd.DataFrame()
-            for i in range(times):
-                run = func(input_generator())
-                run['run'] = i
-                df = pd.concat([df, run], ignore_index=True)
+            with ThreadPool() as pool:
+                for i, run in enumerate(pool.imap(func, (input_generator() for _ in range(times)))):
+                    run['run'] = i
+                    df = pd.concat([df, run], ignore_index=True)
             return df
         return wrapper
     return decorator
 
 
-@run_multiple_times(times=3)
+@run_multiple_times(times=1000)
 def run_simulation(input_data: str):
     simulation_process = subprocess.Popen(["make", "-s", "run-raw", "BIN=simulation", "USE_DOCKER=FALSE",
                                            f'RUN_ARGS=-i /dev/stdin -o /dev/stdout --max-duration 1000'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
@@ -30,7 +32,7 @@ def run_simulation(input_data: str):
     simulation_process.stdin.write(input_data)
     simulation_process.stdin.close()
 
-    analyzer_process = subprocess.Popen(["make", "-s", "run-raw", "BIN=frame_analyzer", "USE_DOCKER=FALSE",
+    analyzer_process = subprocess.Popen(["make", "-s", "run-raw", "BIN=analyze", "USE_DOCKER=FALSE",
                                         f'RUN_ARGS=-i /dev/stdin -o /dev/fd/{simulation_process.stdout.fileno()} -a /dev/stdout'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True, pass_fds=(simulation_process.stdout.fileno(),))
 
     analysis, _ = analyzer_process.communicate(input_data)
@@ -38,8 +40,9 @@ def run_simulation(input_data: str):
     return pd.read_csv(
         StringIO(analysis),
         header=None,
-        names=['t', 'va'],
-        dtype={'t': np.float64, 'va': np.float64}
+        names=['t', 'ball_count', 'kinetic_energy'],
+        dtype={'t': np.float64, 'ball_count': np.uint64,
+               'kinetic_energy': np.float64}
     )
 
 
@@ -49,23 +52,29 @@ def run():
     speed = 0.03
 
     df = pd.DataFrame({
-        'N': pd.Series(dtype=int),
-        'noise': pd.Series(dtype=float),
+        'white_y': pd.Series(dtype=float),
         'run': pd.Series(dtype=int),
         't': pd.Series(dtype=float),
-        'va': pd.Series(dtype=float),
+        'ball_count': pd.Series(dtype=int),
+        'kinetic_energy': pd.Series(dtype=float),
     })
 
-    for N in [40, 100, 400, 1000]:
-        for noise in [1]:
-            print(f"N={N}, noise={noise}")
-            data = run_simulation(lambda :generate(N, L, Rc, noise, speed, None))
-            data['N'] = N
-            data['noise'] = noise
-            df = pd.concat([df, data], ignore_index = False)
+    for white_y in range(42, 56+1):
+        print(f"white_y={white_y}")
+        data = run_simulation(lambda: generate(
+            table_width=224,
+            table_height=112,
+            white_y=white_y,
+            hole_diameter=5.7*2,
+            ball_diameter=5.7,
+            ball_mass=165
+        ))
+        data['white_y'] = white_y
+        df = pd.concat([df, data], ignore_index=False)
 
     df.to_pickle("data/simulation_runs.pkl")
     print(df)
+
 
 if __name__ == "__main__":
     # print(run_simulation(generate(300, 7.0, 0.5, 0.2, 0.03)))
