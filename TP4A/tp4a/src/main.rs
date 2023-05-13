1cefa4629d2de2bf1081c67b17a6b9ba0c3b38ff
Alan Sartorio <alan42ga@hotmail.com>
2023-05-13 17:03:02 -0300

    feat(tp4a): implement gear predictor loop

--- TP4A/tp4a/src/main.rs
+++ TP4A/tp4a/src/main.rs
@@ -1,7 +1,8 @@
-use crate::{beeman::beeman, verlet::verlet};
+use crate::{beeman::beeman, verlet::verlet, gear_predictor_corrector::gear_predictor_corrector};
 
 mod verlet;
 mod beeman;
+mod gear_predictor_corrector;
 
 pub fn euler_algorithm(r: f64, v: f64, f: f64, dt: f64, m: f64) -> (f64, f64) {
     let next_r = r + dt * v + (dt.powi(2) / (2.0 * m)) * f;
@@ -9,65 +10,6 @@ pub fn euler_algorithm(r: f64, v: f64, f: f64, dt: f64, m: f64) -> (f64, f64) {
     let next_v = v + (dt / m) * f;
 
     (next_r, next_v)
-}
-
-fn fitfh_order_gear_corrector_predictor_algorithm<F: FnOnce(f64, f64) -> f64>(
-    r: f64,
-    v: f64,
-    a: f64,
-    r3: f64,
-    r4: f64,
-    r5: f64,
-    dt: f64,
-    m: f64,
-    calculate_force: F,
-) -> (f64, f64, f64, f64, f64, f64) {
-    // Calculate the predictions
-    let r1 = v;
-    let r2 = a;
-
-    let rp = r
-        + r1 * dt
-        + r2 * dt.powi(2) / fac_f64(2)
-        + r3 * dt.powi(3) / fac_f64(3)
-        + r4 * dt.powi(4) / fac_f64(4)
-        + r5 * dt.powi(5) / fac_f64(5);
-    let r1p = r1
-        + r2 * dt
-        + r3 * dt.powi(2) / fac_f64(2)
-        + r4 * dt.powi(3) / fac_f64(3)
-        + r5 * dt.powi(4) / fac_f64(4);
-    let r2p = r2 + r3 * dt + r4 * dt.powi(2) / fac_f64(2) + r5 * dt.powi(3) / fac_f64(3);
-    let r3p = r3 + r4 * dt + r5 * dt.powi(2) / fac_f64(2);
-    let r4p = r4 + r5 * dt;
-    let r5p = r5;
-
-    // Calculate the deltas
-    let da = calculate_force(rp, r1p) / m - r2p; // TODO: validate if this (delta a) is calculated correctly
-    let dr2 = da * dt.powi(2) / fac_f64(2); // delta r2
-
-    // Calculate the corrections
-    let rc = rp + (3.0 / 16.0) * dr2;
-    let vc = r1p + (251.0 / 360.0) * dr2 / dt;
-    let ac = r2p + (1.0) * dr2 * fac_f64(2) / dt.powi(2);
-
-    let r3c = r3p + (11.0 / 18.0) * dr2 * fac_f64(3) / dt.powi(3);
-    let r4c = r4p + (1.0 / 6.0) * dr2 * fac_f64(4) / dt.powi(4);
-    let r5c = r5p + (1.0 / 60.0) * dr2 * fac_f64(5) / dt.powi(5);
-
-    (rc, vc, ac, r3c, r4c, r5c)
-}
-
-const fn fac(n: u64) -> u64 {
-    match n {
-        0u64 | 1u64 => 1,
-        2u64..=20u64 => fac(n - 1u64) * n,
-        _ => 0,
-    }
-}
-
-const fn fac_f64(n: u64) -> f64 {
-    fac(n) as f64
 }
 
 fn analytic_solution(A: f64, gamma: f64, m: f64, t: f64, k: f64) -> f64 {
@@ -91,10 +33,20 @@ fn main() {
     const DT: f64 = 1e-5;
 
     let calc_force = |r: f64, v: f64| -K * r - GAMMA * v;
+    let calc_initial_integration = |r: f64, v: f64| {
+        let r = r;
+        let r1 = v;
+        let r2 = calc_force(r, r1) / M;
+        let r3 = -K * r1 - GAMMA * r2;
+        let r4 = -K * r2 - GAMMA * r3;
+        let r5 = -K * r3 - GAMMA * r4;
+
+        (r, r1, r2, r3, r4, r5)
+    };
 
     let analytic_solution = |t: f64| analytic_solution(A, GAMMA, M, t, K);
 
     //verlet(R, V, calc_force, analytic_solution, DT, M);
-    beeman(R, V, calc_force, analytic_solution, DT, M);
-    //fitfh_order_gear_corrector_predictor_algorithm(r, v, 0.0, 0.0, 0.0, 0.0, dt, calc_force);
+    //beeman(R, V, calc_force, analytic_solution, DT, M);
+    gear_predictor_corrector(R, V, calc_force, calc_initial_integration, analytic_solution, DT, M);
 }
