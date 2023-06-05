cc9bc65e0732448d15abd491dcacfaf81b8249f6
Alan Sartorio <alan42ga@hotmail.com>
2023-06-05 04:57:00 -0300

    feat(tp5): add simulation runner

--- TP5/scripts/run_simulation.py
+++ TP5/scripts/run_simulation.py
@@ -25,47 +25,44 @@ def run_multiple_times(times: int):
 @run_multiple_times(times=3)
 def run_simulation(input_data: str):
     simulation_process = subprocess.Popen(["make", "-s", "run-raw", "BIN=simulation", "USE_DOCKER=FALSE",
-                                           f'RUN_ARGS=-i /dev/stdin -o /dev/stdout --max-duration 1000'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
+                                           f'RUN_ARGS=--input /dev/stdin --output-exit-times=/dev/stdout --output-particles=/dev/null --outputs-per-second=1'],
+                                          stdout=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
 
-    simulation_process.stdin.write(input_data)
-    simulation_process.stdin.close()
+    data, _ = simulation_process.communicate(input_data)
+    data = list(map(float, data.splitlines()))
 
-    analyzer_process = subprocess.Popen(["make", "-s", "run-raw", "BIN=frame_analyzer", "USE_DOCKER=FALSE",
-                                        f'RUN_ARGS=-i /dev/stdin -o /dev/fd/{simulation_process.stdout.fileno()} -a /dev/stdout'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True, pass_fds=(simulation_process.stdout.fileno(),))
-
-    analysis, _ = analyzer_process.communicate(input_data)
-
-    return pd.read_csv(
-        StringIO(analysis),
-        header=None,
-        names=['t', 'va'],
-        dtype={'t': np.float64, 'va': np.float64}
-    )
+    return pd.DataFrame(enumerate(data), columns=['exit_n', 'time'])
 
 
 def run():
-    L = 20
-    Rc = 0.5
-    speed = 0.03
-
     df = pd.DataFrame({
         'N': pd.Series(dtype=int),
-        'noise': pd.Series(dtype=float),
+        'd': pd.Series(dtype=float),
         'run': pd.Series(dtype=int),
-        't': pd.Series(dtype=float),
-        'va': pd.Series(dtype=float),
+        'exit_n': pd.Series(dtype=int),
+        'time': pd.Series(dtype=float),
     })
 
-    for N in [40, 100, 400, 1000]:
+    for N, d in zip([200, 260, 320, 380], [1.2, 1.8, 2.4, 3.0]):
         for noise in [1]:
-            print(f"N={N}, noise={noise}")
-            data = run_simulation(lambda :generate(N, L, Rc, noise, speed, None))
+            print(f"N={N}, d={noise}")
+            data = run_simulation(lambda: generate(
+                n=N,
+                room_side=20,
+                exit_size=d,
+                far_exit_distance=10,
+                far_exit_size=3,
+                max_speed=2,
+                min_radius=0.1,
+                max_radius=0.32,
+            ))
             data['N'] = N
-            data['noise'] = noise
-            df = pd.concat([df, data], ignore_index = False)
+            data['d'] = d
+            df = pd.concat([df, data], ignore_index=False)
 
-    df.to_pickle("data/simulation_runs_b.pkl")
+    df.to_csv("data/simulation_b.csv", index=False)
     print(df)
+
 
 if __name__ == "__main__":
     # print(run_simulation(generate(300, 7.0, 0.5, 0.2, 0.03)))
